64fdd284f9b7d3ba168a5485e7431f8ccc3d1d39
Davsav <davidbyu6@gmail.com>
2021-08-03 21:23:14 -0600

    added offline capabilities

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
+import { idbPromise } from '../utils/helpers';
 
 import Cart from '../components/Cart';
 import { useStoreContext } from "../utils/GlobalState";
@@ -31,8 +32,19 @@ useEffect(() => {
       type: UPDATE_PRODUCTS,
       products: data.products
     });
+
+    data.products.forEach((product) => {
+      idbPromise('products', 'put', product);
+    });
+  } else if (!loading) {
+    idbPromise('products', 'get').then((indexedProducts) => {
+      dispatch({
+        type: UPDATE_PRODUCTS,
+        products: indexedProducts
+      });
+    });
   }
-}, [products, data, dispatch, id]);
+}, [products, data, loading, dispatch, id]);
 
 const addToCart = () => {
   const itemInCart = cart.find((CartItem) => CartItem._id === id);
@@ -42,11 +54,16 @@ const addToCart = () => {
       _id: id,
       purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
     });
+    idbPromise('cart', 'put', {
+      ...itemInCart,
+      purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+    });
   } else {
     dispatch({
       type: ADD_TO_CART,
       product: { ...currentProduct, purchaseQuantity: 1}
     });
+    idbPromise('cart', 'put', { ...currentProduct, purchaseQuantity: 1 });
   }
 };
 
@@ -55,6 +72,7 @@ const removeFromCart = () => {
     type: REMOVE_FROM_CART,
     _id: currentProduct._id
   });
+  idbPromise('cart', 'delete', { ...currentProduct });
 };
 
   return (
